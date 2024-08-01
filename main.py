c2d1ff23814f853d8b519c2c0fb63d02dda8dea6
Vivek Kumar <vivekkumarpaneer3@gmail.com>
2024-08-02 00:34:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import numpy as np
 import joblib
  
@@ -6,14 +7,31 @@ loaded_model = joblib.load('linear_regression_model.h5')
  
 app = FastAPI()
  
+class Features(BaseModel):
+    Soil_Quality: float
+    Seed_Variety: float
+    Fertilizer_Amount_kg_per_hectare: float
+    Sunny_Days: float
+    Rainfall_mm: float
+    Irrigation_Schedule: float
+ 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
 
 # Define the prediction endpoint
 @app.post("/predict/")
-async def predict(features: list): 
-    custom_sample = np.array([features], dtype=np.float32)
+async def predict(data: Features): 
+    custom_sample = np.array([[
+        data.Soil_Quality,
+        data.Seed_Variety,
+        data.Fertilizer_Amount_kg_per_hectare,
+        data.Sunny_Days,
+        data.Rainfall_mm,
+        data.Irrigation_Schedule
+    ]], dtype=np.float32)
+     
     predictions = loaded_model.predict(custom_sample)
     predicted_yield = predictions[0]
+    
     return {"predicted_yield": f"{predicted_yield:.2f}"}
